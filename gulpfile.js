73e8d80fd4fad5b4857da5357b372a89ea36e473
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-03-15 16:28:27 -0500

    Added gulp style task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
+// Require Modules
 var gulp = require('gulp');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+
+// Set Variables
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+// Gulp Tasks
+gulp.task('style', function () {
+    gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+});
