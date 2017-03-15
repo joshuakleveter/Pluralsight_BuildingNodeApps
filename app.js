06051d4b3ef29a7655e88e4f1c6e8a0524d4f905
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-03-15 14:27:10 -0500

    Added app.listen() call

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+// Require modules
 var express = require('express');
+
+// Set variables
+var port = 5000;
+var app = express();
+
+// App code
+app.listen(port, function (err) {
+    console.log(`Running server on port ${port}`);
+});
