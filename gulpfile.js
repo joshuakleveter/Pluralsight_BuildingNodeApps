e6bf9020045f56756736fc6315cd980e22a55ad6
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-03-16 15:51:30 +0000

    Added inject task

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,24 @@
 var gulp   = require('gulp');
 var jscs   = require('gulp-jscs');
 var jshint = require('gulp-jshint');
+var wiredep = require('wiredep').stream;
 
 // Set Variables
 var jsFiles = ['*.js', 'src/**/*.js'];
 
 // Gulp Tasks
+gulp.task('inject', function () {
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public/'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views/'));
+});
+
 gulp.task('style', function () {
     gulp.src(jsFiles)
         .pipe(jshint())
